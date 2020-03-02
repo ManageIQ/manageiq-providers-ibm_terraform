356bee28b78d1ac9f62673e7707c6a16150fe312
Adam Grare <agrare@redhat.com>
2020-03-02 13:49:29 -0500

    Add a simple Refresher spec test
    Add specs for a ConfigurationManager Refresh test pending API calls and
    VCR cassettes

--- spec/models/manageiq/providers/cloud_automation_manager/configuration_manager/refresher_spec.rb
+++ spec/models/manageiq/providers/cloud_automation_manager/configuration_manager/refresher_spec.rb
@@ -1,2 +1,27 @@
 describe ManageIQ::Providers::CloudAutomationManager::ConfigurationManager::Refresher do
+  it ".ems_type" do
+    expect(described_class.ems_type).to eq(:cam_configuration)
+  end
+
+  context "#refresh" do
+    let(:provider) do
+      url = Rails.application.secrets.cam.try(:[], 'url') || 'CLOUD_AUTOMATION_MANAGER_URL'
+      FactoryBot.create(:provider_cam, :url => url).tap do |p|
+        userid   = Rails.application.secrets.cam.try(:[], 'user') || 'CLOUD_AUTOMATION_MANAGER_USER'
+        password = Rails.application.secrets.cam.try(:[], 'password') || 'CLOUD_AUTOMATION_MANAGER_PASSWORD'
+
+        p.update_authentication(:default => {:userid => userid, :password => password})
+      end
+    end
+
+    let(:ems) { provider.configuration_manager }
+
+    it "will perform a full refresh" do
+      2.times do
+        VCR.use_cassette(described_class.name.underscore) do
+          EmsRefresh.refresh(ems)
+        end
+      end
+    end
+  end
 end
