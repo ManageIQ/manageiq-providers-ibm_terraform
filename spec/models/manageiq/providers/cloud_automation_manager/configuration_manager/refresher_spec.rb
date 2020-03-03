23c5080265cf945bf2862f3a486a98d556642d8d
Adam Grare <agrare@redhat.com>
2020-03-03 11:18:04 -0500

    Fix Rails secrets constants

--- spec/models/manageiq/providers/cloud_automation_manager/configuration_manager/refresher_spec.rb
+++ spec/models/manageiq/providers/cloud_automation_manager/configuration_manager/refresher_spec.rb
@@ -5,10 +5,10 @@ describe ManageIQ::Providers::CloudAutomationManager::ConfigurationManager::Refr
 
   context "#refresh" do
     let(:provider) do
-      url = Rails.application.secrets.cam.try(:[], :url) || 'CLOUD_AUTOMATION_MANAGER_URL'
-      FactoryBot.create(:provider_cam, :url => url).tap do |p|
-        userid   = Rails.application.secrets.cam.try(:[], :user) || 'CLOUD_AUTOMATION_MANAGER_USER'
-        password = Rails.application.secrets.cam.try(:[], :password) || 'CLOUD_AUTOMATION_MANAGER_PASSWORD'
+      url = Rails.application.secrets.cam.try(:[], :url) || 'CAM_URL'
+      FactoryBot.create(:provider_cam, :url => "https://#{url}").tap do |p|
+        userid   = Rails.application.secrets.cam.try(:[], :user) || 'CAM_USER'
+        password = Rails.application.secrets.cam.try(:[], :password) || 'CAM_PASSWORD'
 
         p.update_authentication(:default => {:userid => userid, :password => password})
       end
