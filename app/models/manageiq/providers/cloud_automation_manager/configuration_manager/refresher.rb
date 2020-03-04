a3e88d0b316a77b7a28bd4ac9f9dae5880a27f62
Adam Grare <agrare@redhat.com>
2020-03-04 09:13:06 -0500

    Use encode_www_form for query parameter

--- app/models/manageiq/providers/cloud_automation_manager/configuration_manager/refresher.rb
+++ app/models/manageiq/providers/cloud_automation_manager/configuration_manager/refresher.rb
@@ -44,7 +44,7 @@ module ManageIQ::Providers
         template_uri = URI.parse(base_url)
         template_uri.port = 30000
         template_uri.path = "/cam/api/v1/templates"
-        template_uri.query = "tenantId=#{tenant_id}&ace_orgGuid=#{all}&cloudOE_spaceGuid=#{team}"
+        template_uri.query = URI.encode_www_form("tenantId" => tenant_id, "ace_orgGuid" => all, "cloudOE_spaceGuid" => team)
 
         #template_uri_str = base_url + ":30000/cam/api/v1/stacks?tenantId=" + tenant_id + "&ace_orgGuid=" + all + "&cloudOE_spaceGuid=" + team
         template_body = redirect_cam_api(template_uri.to_s, 5, connection)
