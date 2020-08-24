dac78730732a622bd4710d081e7505604f4f9ea6
Adam Grare <agrare@redhat.com>
2020-08-24 17:56:12 -0400

    Update DDF new provider forms

--- app/models/manageiq/providers/ibm_terraform/configuration_manager.rb
+++ app/models/manageiq/providers/ibm_terraform/configuration_manager.rb
@@ -5,13 +5,21 @@ class ManageIQ::Providers::IbmTerraform::ConfigurationManager < ManageIQ::Provid
   require_nested :RefreshWorker
 
   include ProcessTasksMixin
-  delegate :authentication_check,
+  delegate :authentications,
+           :authentications=,
+           :authentication_check,
            :authentication_status,
            :authentication_status_ok?,
            :connect,
+           :endpoints,
+           :endpoints=,
+           :url,
+           :url=,
            :verify_credentials,
            :with_provider_connection,
            :to => :provider
+
+  belongs_to :provider, :autosave => true, :dependent => :destroy
 
   class << self
     delegate :params_for_create,
@@ -33,5 +41,47 @@ class ManageIQ::Providers::IbmTerraform::ConfigurationManager < ManageIQ::Provid
 
   def self.display_name(number = 1)
     n_('Configuration Manager (IBM Terraform)', 'Configuration Managers (IBM Terraform)', number)
+  end
+
+  def self.create_from_params(params, endpoints, authentications)
+    new(params).tap do |ems|
+      endpoints.each { |endpoint| ems.assign_nested_endpoint(endpoint) }
+      authentications.each { |authentication| ems.assign_nested_authentication(authentication) }
+
+      ems.provider.save!
+      ems.save!
+    end
+  end
+
+  def edit_with_params(params, endpoints, authentications)
+    tap do |ems|
+      transaction do
+        # Remove endpoints/attributes that are not arriving in the arguments above
+        ems.endpoints.where.not(:role => nil).where.not(:role => endpoints.map { |ep| ep['role'] }).delete_all
+        ems.authentications.where.not(:authtype => nil).where.not(:authtype => authentications.map { |au| au['authtype'] }).delete_all
+
+        ems.assign_attributes(params)
+        ems.endpoints = endpoints.map(&method(:assign_nested_endpoint))
+        ems.authentications = authentications.map(&method(:assign_nested_authentication))
+
+        ems.provider.save!
+        ems.save!
+      end
+    end
+  end
+  delegate :name=, :zone, :zone=, :zone_id, :zone_id=, :to => :provider
+
+  def name
+    "#{provider.name} Configuration Manager"
+  end
+
+  def provider
+    super || ensure_provider
+  end
+
+  private
+
+  def ensure_provider
+    build_provider.tap { |p| p.configuration_manager = self }
   end
 end
