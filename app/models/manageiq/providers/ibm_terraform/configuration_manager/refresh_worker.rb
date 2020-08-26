c856bc59dc4a7b25454483c6061e27acbd1a8ede
Adam Grare <agrare@redhat.com>
2020-08-26 16:53:41 -0400

    Fix refresh_worker settings name

--- app/models/manageiq/providers/ibm_terraform/configuration_manager/refresh_worker.rb
+++ app/models/manageiq/providers/ibm_terraform/configuration_manager/refresh_worker.rb
@@ -1,7 +1,3 @@
 class ManageIQ::Providers::IbmTerraform::ConfigurationManager::RefreshWorker < MiqEmsRefreshWorker
   require_nested :Runner
-
-  def self.settings_name
-    :ems_refresh_worker_cam_configuration
-  end
 end
