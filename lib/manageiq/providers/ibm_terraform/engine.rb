8e4b50e5fd675b24f760bbecee5094f169e272ac
Jason Frey <fryguy9@gmail.com>
2020-09-11 16:08:43 -0400

    Plugin Generator Updates

--- lib/manageiq/providers/ibm_terraform/engine.rb
+++ lib/manageiq/providers/ibm_terraform/engine.rb
@@ -11,7 +11,7 @@ module ManageIQ
         end
 
         def self.plugin_name
-          _('ManageIQ Providers IBM Terraform')
+          _('IBM Terraform Provider')
         end
       end
     end
