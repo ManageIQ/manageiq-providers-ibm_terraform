7a6d285c6dd409c528df771ab7d478da9d16ae01
Kenneth Chu <chukenne@ca.ibm.com>
2020-11-10 12:26:15 -0500

    Update Azure ems_ref to match Azure Provider

--- spec/models/manageiq/providers/ibm_terraform/configuration_manager/refresher_spec.rb
+++ spec/models/manageiq/providers/ibm_terraform/configuration_manager/refresher_spec.rb
@@ -6,7 +6,9 @@ describe ManageIQ::Providers::IbmTerraform::ConfigurationManager::Refresher do
   context "#refresh" do
     before { EvmSpecHelper.create_guid_miq_server_zone }
 
-    let!(:cross_link_vm) { FactoryBot.create(:vm, :ems_ref => "i-0361c15366e550109", :uid_ems => "i-0361c15366e550109") }
+    let!(:cross_link_aws_vm) { FactoryBot.create(:vm, :ems_ref => "i-0361c15366e550109", :uid_ems => "i-0361c15366e550109") }
+    let!(:cross_link_azure_vm) { FactoryBot.create(:vm, :ems_ref => "0e0a4287-8719-4849-bb0b-5242e4507709/virtualmachine-eff8898f-rg/microsoft.compute/virtualmachines/virtualmachine-vm", :uid_ems => "b70ad672-e302-4124-b771-f7f2e64dc6f8") }
+    let!(:cross_link_vmware_vm) { FactoryBot.create(:vm, :ems_ref => "vm-41321", :uid_ems => "421bb662-77fb-a66e-3fd9-ff0cdc7578b1") }
 
     let(:zone) { FactoryBot.create(:zone) }
     let(:params) { {:name => "IbmTerraform for test", :zone_id => zone.id} }
@@ -38,13 +40,15 @@ describe ManageIQ::Providers::IbmTerraform::ConfigurationManager::Refresher do
         assert_ems_counts
         configuration_profile_id = assert_specific_configuration_profile
         orchestration_stack_id = assert_specific_orchestration_stack
-        assert_specific_configured_system(configuration_profile_id, orchestration_stack_id)
+        assert_specific_aws_configured_system(configuration_profile_id, orchestration_stack_id)
+        assert_specific_azure_configured_system
+        assert_specific_vmware_configured_system
       end
     end
 
     def assert_ems_counts
       expect(ems.configuration_profiles.count).to eq(169)
-      expect(ems.configured_systems.count).to     eq(2)
+      expect(ems.configured_systems.count).to     eq(4)
     end
 
     def assert_specific_configuration_profile
@@ -70,9 +74,9 @@ describe ManageIQ::Providers::IbmTerraform::ConfigurationManager::Refresher do
       orchestration_stack.id
     end
 
-    def assert_specific_configured_system(configuration_profile_id, orchestration_stack_id)
-      configured_system = ems.configured_systems.find_by(:manager_ref => "5eac8d80ed4fa000171eaa23")
-      expect(configured_system).to have_attributes(
+    def assert_specific_aws_configured_system(configuration_profile_id, orchestration_stack_id)
+      aws_configured_system = ems.configured_systems.find_by(:manager_ref => "5eac8d80ed4fa000171eaa23")
+      expect(aws_configured_system).to have_attributes(
         :type                     => "ManageIQ::Providers::IbmTerraform::ConfigurationManager::ConfiguredSystem",
         :hostname                 => "demoinstance",
         :vendor                   => "Amazon EC2",
@@ -82,7 +86,29 @@ describe ManageIQ::Providers::IbmTerraform::ConfigurationManager::Refresher do
         :console_url              => "https://#{url}/cam/instances/#!/instanceDetails/5eac8d41ed4fa000171eaa1b"
       )
 
-      expect(configured_system.counterpart).to eq(cross_link_vm)
+      expect(aws_configured_system.counterpart).to eq(cross_link_aws_vm)
+    end
+
+    def assert_specific_azure_configured_system
+      azure_configured_system = ems.configured_systems.find_by(:manager_ref => "5fad64b05456fc0018395307")
+      expect(azure_configured_system).to have_attributes(
+        :type                 => "ManageIQ::Providers::IbmTerraform::ConfigurationManager::ConfiguredSystem",
+        :vendor               => "Microsoft Azure",
+        :virtual_instance_ref => "0e0a4287-8719-4849-bb0b-5242e4507709/virtualmachine-eff8898f-rg/microsoft.compute/virtualmachines/virtualmachine-vm"
+      )
+
+      expect(azure_configured_system.counterpart).to eq(cross_link_azure_vm)
+    end
+
+    def assert_specific_vmware_configured_system
+      vmware_configured_system = ems.configured_systems.find_by(:manager_ref => "5f9c52b4c4e5ae00180fb96a")
+      expect(vmware_configured_system).to have_attributes(
+        :type                 => "ManageIQ::Providers::IbmTerraform::ConfigurationManager::ConfiguredSystem",
+        :vendor               => "VMware vSphere",
+        :virtual_instance_ref => "421bb662-77fb-a66e-3fd9-ff0cdc7578b1"
+      )
+
+      expect(vmware_configured_system.counterpart).to eq(cross_link_vmware_vm)
     end
   end
 end
